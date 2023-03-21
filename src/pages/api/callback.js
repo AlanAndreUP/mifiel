00c978fa880b7d10b41bb2c82dba90901dd59d8f
Alan Andre <114703935+AlanAndreUP@users.noreply.github.com>
2023-03-21 17:05:11 -0600

    Add files via upload

---
+++ src/pages/api/callback.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  // Aquí deberías manejar la información que recibes del servidor remoto
+
+  // Ejemplo de cómo obtener información de la petición
+  const { query, body, headers } = req;
+
+  // Aquí deberías realizar la lógica necesaria para manejar la información recibida
+  // y enviar una respuesta adecuada al cliente
+
+  // Ejemplo de cómo enviar una respuesta de éxito con un mensaje
+  res.status(200).json({ message: 'Información recibida correctamente' });
+}
